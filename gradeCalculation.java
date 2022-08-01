5a15b0d06196be451eac3c45a0fd51956b4659b0
Samrrutha <90636310+Samrrutha@users.noreply.github.com>
2022-08-01 19:47:39 +0530

    Create gradeCalculation.java
    Enter the mark of the student it will give you the grade.

---
+++ gradeCalculation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+public static void main(String args[])
+{
+Scanner sc  = new Scanner(System.in);
+int avg=nextInt();
+System.out.print("The student Grade is: ");
+if(avg>=80)
+{
+System.out.print("A");
+}
+else if(avg>=60 && avg<80)
+{
+System.out.print("B");
+} 
+else if(avg>=40 && avg<60)
+{
+System.out.print("C");
+}
+else
+{
+System.out.print("D");
+}
+}
+}
